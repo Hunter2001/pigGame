e57187d901e54dccb88c13193f4596bc9b79ca2b
Hunter2001 <34722326+Hunter2001@users.noreply.github.com>
2022-03-12 21:04:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,54 @@
 'use strict';
 // -----------------------------------SELECTING ELEMENTS-------------------------------------------------
+// PLAYER ELEMENTS
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
-
+// SCORE ELEMENTS
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-
+// CURRENT SCORE ELEMENTS
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+// DICE ELEMENT
 const diceEl = document.querySelector('.dice');
-
+// BUTTONS ELEMENTS
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // ---------------------------------STARTING CONDITIONS FUNCTION------------------------------------------
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let gameOn = true;
-
+// DEFINE VARIABLES
+let scores, currentScore, activePlayer, gameOn;
+// FUNCTIONS ITSELF
 const baseConditions = function () {
-  scores[0] = 0;
-  scores[1] = 0;
+  // SET VARIABLES
+  scores = [0, 0];
   currentScore = 0;
-  document
-    .querySelector(`.player--${activePlayer}`)
-    .classList.remove('player--winner');
-  document
-    .querySelector(`.player--${activePlayer}`)
-    .classList.add('player--active');
-  if (activePlayer === 1) {
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
-  }
   activePlayer = 0;
+  gameOn = true;
+  // SET INTERFACE TO INIT
+  document.querySelector(`.player--0`).classList.remove('player--winner');
+  document.querySelector(`.player--1`).classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  // SET VALUES TO ZERO
   score0El.textContent = 0;
   score1El.textContent = 0;
   current0El.textContent = 0;
   current1El.textContent = 0;
+  // REMOVE THE DICE
   diceEl.classList.add('hidden');
-  gameOn = true;
 };
-
+// CALL THE FUNCTIONS IN THE BEGINING
 baseConditions();
 
 // -------------------------------------SWITCH PLAYER FUNCTION-------------------------------------------------
 const switchPlayer = function () {
-  // anihilate current score
+  // SET CURRENT SCORE TO ZERO AND WRITE IT
   currentScore = 0;
   document.getElementById(`current--${activePlayer}`).textContent =
     currentScore;
-  //   switch player
+  // SWITCH PLAYER
   activePlayer = activePlayer === 1 ? 0 : 1;
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
@@ -61,13 +58,12 @@ const switchPlayer = function () {
 
 btnRoll.addEventListener('click', function () {
   if (gameOn) {
-    // generate the random number
+    // GENERATE THE RANDOM NUMBER
     const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice);
-    // display dice roll
+    // DISPLAY DICE ROLL
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
-    // if dice = 1 -> switch player
+    // CHECK IF DICE !== 1
     if (dice !== 1) {
       currentScore += dice;
       document.getElementById(`current--${activePlayer}`).textContent =
@@ -82,13 +78,13 @@ btnRoll.addEventListener('click', function () {
 
 btnHold.addEventListener('click', function () {
   if (gameOn) {
-    // add current score to total score
+    // ADD CURRENT SCORE TO TOTAL SCORE
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    // check if score > 100
-    if (scores[activePlayer] >= 50) {
+    // CHECK IF SCORE > 100
+    if (scores[activePlayer] >= 100) {
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
@@ -104,7 +100,4 @@ btnHold.addEventListener('click', function () {
 });
 
 // ---------------------------------------------NEW GAME BUTTON-----------------------------------------
-btnNew.addEventListener('click', function () {
-  console.log(`button new was clicked`);
-  baseConditions();
-});
+btnNew.addEventListener('click', baseConditions);
